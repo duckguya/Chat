12e8b16e55de40f33db98db178eaef131e3250d9
duckguya <duckguya@gmail.com>
2023-01-20 05:07:38 +0900

    dotenv

--- main/firebase.tsx
+++ main/firebase.tsx
@@ -3,7 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
